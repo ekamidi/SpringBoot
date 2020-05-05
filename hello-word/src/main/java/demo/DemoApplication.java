ed84c206908d104f604e0c23319e49465fc82426
Kamel Idir <Kamel.Idir@ericsson.com>
2020-05-05 18:01:27 -0400

    deleted log file

--- hello-word/src/main/java/demo/DemoApplication.java
+++ hello-word/src/main/java/demo/DemoApplication.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoApplication {
 	
-	
 	@GetMapping(value="/hello")
 	public String getMethodNameString() {
 
